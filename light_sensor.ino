4ba21db0cf7502536e852d066b497e678ef83a38
김아현 <dhflxhdxhd@gmail.com>
2021-11-16 09:58:21 +0900

    light_onOff
    light sensor add onOff function

--- light_sensor.ino
+++ light_sensor.ino
@@ -6,5 +6,12 @@ void setup() {
 void loop() {
    Serial.print("the light value is:");
    int lightValue = analogRead(lightIn);
+   Serial.println(lightValue);
+   onOff(lightValue);
    delay(3000);
+}
+
+void onOff(int lightValue){
+  if(lightValue < 300)digitalWrite(LED,HIGH);
+  else digitalWrite(LED,LOW);  
 }
