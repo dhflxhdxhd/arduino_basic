2e121f9d3fa62af9527d421678e707920611d315
김아현 <dhflxhdxhd@gmail.com>
2021-11-16 09:56:56 +0900

    DHT sensor
    temp&humidity

---
+++ DHT_temp_humi.ino
@@ -0,0 +1,19 @@
+#include<DHT.h>
+int Sensor_pin = 8;
+DHT my_sensor(Sensor_pin,DHT11);
+void setup() {
+  Serial.begin(9600);
+  my_sensor.begin();
+}
+
+void loop() {
+  delay(500);
+  int temp = my_sensor.readTemperature();
+  int humi = my_sensor.readHumidity();
+
+  Sriaal.print("Temperature:");
+  Serial.print(temp);
+  Serial.print("/");
+  Serial.print("Humidity:");
+  Serail.println(humi);
+}
